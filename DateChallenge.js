b03c305f25e410f14c83cebb200a38b461310a56
Berkay Cataltug <bcataltug@gmail.com>
2025-03-02 19:55:13 +0300

    Added isWeekend solution

--- DateChallenge.js
+++ DateChallenge.js
@@ -7,5 +7,13 @@ function calculateAge(date) {
     return age;
     
   }
+
+  function isWeekend(date){
+    const givenDate = new Date(date);
+    const day = givenDate.getDay();
+
+    return day === 0 || day === 6;
+  }
   
   console.log(calculateAge("1990-06-15")); // farkli tarih formatlariyla fonksiyonunuzu test edin
+  console.log(isWeekend("2020-01-18")); // farkli tarih formatlariyla fonksiyonunuzu test edin
